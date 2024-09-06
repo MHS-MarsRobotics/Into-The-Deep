df68539c04f4bbd8d5afb45c525461096a01a192
oswald <oswaldaviles358@gmail.com>
2024-09-06 11:54:16 -0500

    Last Season Stuff

---
+++ Team6206/src/main/java/org/firstinspires/ftc/team6206/Auton_1.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.team6206;
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.team6206.MecanumDrive;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Config
+@Autonomous(name = "Red Auto", group = "Autonomous")
+public class Auton_1 extends LinearOpMode {
+    MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-35, -62, Math.toRadians(90)));
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Action trajectoryAction1 = drive.actionBuilder(drive.pose)
+                .splineTo(new Vector2d(-35, -35), Math.toRadians(90))
+                .build();
+
+
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryAction1
+                )
+        );
+    }
+}
