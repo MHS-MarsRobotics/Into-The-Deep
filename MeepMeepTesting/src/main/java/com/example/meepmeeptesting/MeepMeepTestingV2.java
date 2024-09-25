cb2913e586eeea9f93f1e26629cc79c12934aa27
Oswald <oswaldaviles@students-marionunit2.org>
2024-09-25 16:57:02 -0500

    Ato plan v2

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingV2.java
@@ -0,0 +1,67 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MeepMeepTestingV2 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(32, -62, Math.toRadians(90)))
+                                .lineTo(new Vector2d(0,-37))
+                                .waitSeconds(0.5)
+                                .lineTo(new Vector2d(0,-39))
+                                .lineTo(new Vector2d(48, -39))
+                                .lineTo(new Vector2d(48,-36))
+                                .waitSeconds(0.5)
+                                .turn(Math.toRadians(90))
+                                .waitSeconds(0.5)
+                                .lineTo(new Vector2d(48,-39))
+                                .build());
+
+
+        /*RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setColorScheme(new ColorSchemeRedDark()).setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(35, -62, Math.toRadians(90)))
+
+                        .splineTo (new Vector2d(56,  -40), Math.toRadians(90))
+                        .waitSeconds(1)
+                        .splineTo (new Vector2d(57, 38), Math.toRadians(90))
+                        .turn(Math. toRadians(180))
+                        .splineTo (new Vector2d(32, -44), Math. toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-43, -50, Math.toRadians(-110)), Math.toRadians(180))
+                        .build());*/
+
+
+
+
+        Image img = null;
+        try {
+            img = ImageIO.read(new File("MeepMeepTesting/src/main/java/com/example/meepmeeptesting/field-2024-juice-dark.png"));
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        meepMeep.setBackground(img)
+//  <following code you were using previously>
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)//.addEntity(mySecondBot).start()
+                .start();
+    }
+}
