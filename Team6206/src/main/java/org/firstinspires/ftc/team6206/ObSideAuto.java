d0e93881810282a91b20e6d84f78706c9ac32347
Ozieboy-358 <99374871+Ozieboy-358@users.noreply.github.com>
2024-09-26 15:55:20 -0500

    week 3 auto name change

--- Team6206/src/main/java/org/firstinspires/ftc/team6206/ObSideAuto.java
+++ Team6206/src/main/java/org/firstinspires/ftc/team6206/ObSideAuto.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.team6206;
-import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -9,35 +7,10 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.team6206.MecanumDrive;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.imgproc.Moments;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Config
-@Autonomous(name = "Red Auto", group = "Autonomous")
-public class Auton_1 extends LinearOpMode {
+@Autonomous(name = "Observation-Side Auto", group = "Autonomous")
+public class ObSideAuto extends LinearOpMode {
 
 
 
